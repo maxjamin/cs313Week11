effbdb41b3ab3e70b120c7b1afe1af764ef36707
maxjamin <maxjamin@gmail.com>
2019-03-15 16:27:57 -0600

    Adding main page

---
+++ views/pages/clientsideMain.js
@@ -0,0 +1,12 @@
+	function getUser() {
+		console.log("getUser called");
+
+		var username = $("#userName").val();
+		var password = $("#password").val();
+		console.log("The user" +  username + password);
+
+		$.get("/logInUser", {userName:username, password:password}, function(data) {
+			console.log("DATA RETURNED: " + data);
+
+		})
+	}
