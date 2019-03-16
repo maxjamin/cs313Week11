4de21e2356d725d487249e43a206cc55809c5e28
maxjamin <maxjamin@gmail.com>
2019-03-16 12:48:30 -0600

    Adding main page

--- public/clientsideMain.js
+++ public/clientsideMain.js
@@ -41,7 +41,14 @@ function loadProductTable(results) {
 
 	var Table = document.getElementById("productTable");
 
-	console.log("length " + results.length);
+	for(var i=0; i<results.length; i++) {
+		var row = table.insertRow(0);
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+		cell1.innerHTML = "NEW CELL1";
+		cell2.innerHTML = "NEW CELL2"; 
+	}
+
 }
 
 
