900e4fc8c7517f15e46a6fc96ca28a9b512f8a0a
maxjamin <maxjamin@gmail.com>
2019-03-16 14:46:21 -0600

    Adding main page

--- public/clientsideMain.js
+++ public/clientsideMain.js
@@ -51,7 +51,7 @@ function loadProductTable(results) {
 		var linktoart = "/artWorkImages/" + results[i].linktoart;
 		var product = [results[i].name, results[i].description, 
 		"<img src='" + linktoart + "' width='250' height='250'>",
-		results[i].price, results[i].quantity, "<button value="results[i].name" onclick='addToCart()'>+</button>"];
+		results[i].price, results[i].quantity, "<button value="+results[i].name+" onclick='addToCart()'>+</button>"];
 		var prod = table.insertRow(i);
 
 		//Add items to the row.
