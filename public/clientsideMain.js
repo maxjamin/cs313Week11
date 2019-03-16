ea9e4aa1a8241ff96315e83244fbca6569cc2fa2
maxjamin <maxjamin@gmail.com>
2019-03-16 11:00:01 -0600

    Adding main page

--- public/clientsideMain.js
+++ public/clientsideMain.js
@@ -1,34 +1,34 @@
 
 
 function checkIfLoggedIn() {
-	var email = localStorage.getItem("powerFull");
-	if(email != "")
+	var username = localStorage.getItem("userLogin");
+	if(username != "")
 	{
 		console.log("User exists");
 
-		//$("#userLoginBar").append(data.username + " logged in");
+		$("#userLoginBar").append(username + " logged in");
 		document.getElementById("loginForm").style.display = "none";
 	}
 }
+
 
 function getUser() {
 		console.log("getUser called");
 
 		var username = $("#userName").val();
 		var password = $("#password").val();
-		var x;
 		console.log("The user" +  username + password);
 
 		$.get("/logInUser", {userName:username, password:password}, function(data) {
 			console.log("DATA RETURNED: " + JSON.stringify(data));
-			x = data;
 			console.log("Test01: " + x);
 			
 			$("#userLoginBar").append(data.username + " logged in");
 
 			/*If user is their, log in and hide log in form*/
 			document.getElementById("loginForm").style.display = "none";
-			localStorage.setItem(data.username,data.email);
+			localStorage.setItem("userLogin", data.username);
+			localStorage.setItem("userEmail", data.email);
 
 		})
 
