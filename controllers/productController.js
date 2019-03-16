094de5849b2f0df5c6b52e507eaee23807ea9340
maxjamin <maxjamin@gmail.com>
2019-03-16 13:16:20 -0600

    Adding main page

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,8 +9,6 @@ function getProducts(request, response) {
 			console.log("ERROR " + error + " Result " + result + " Length: " + result.length);
 			response.status(500).json({success: false, data: error});
 		} else {
-			console.log("RESULT ARE: " + result);
-			
 			//response.write(JSON.stringify(person));
 			response.json(result);
 		}
