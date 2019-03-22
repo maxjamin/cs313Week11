b8c5347221ab1a13dfe164f96fc52b1d2daba604
maxjamin <maxjamin@gmail.com>
2019-03-22 15:46:55 -0600

    add sessions

--- public/clientsideMain.js
+++ public/clientsideMain.js
@@ -27,8 +27,8 @@ function getUser() {
 		$.get("/logInUser", {userName:username, password:password}, function(data) {
 			console.log("DATA RETURNED: " + JSON.stringify(data));
 			
-			
-			document.getElementById("userLoginBar").innerHTML = data.username + " logged in";
+
+			document.getElementById("userLoginBar").innerHTML = " logged in";
 			document.getElementById("loginForm").style.display = "none";
 			//If user is their, log in and hide log in form
 			/*document.getElementById("loginForm").style.display = "none";
