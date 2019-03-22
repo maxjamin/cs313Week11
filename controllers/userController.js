e30ca07754dcf2999688894b3cce4716de9029eb
maxjamin <maxjamin@gmail.com>
2019-03-22 13:35:46 -0600

    add sessions

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,8 +16,8 @@ function getPerson(request, response) {
 		} else {
 			var person = result[0];
 			
-			request.session.user = person.username;
-			console.log("Session " + request.session.user);
+			//request.session.user = person.username;
+			//console.log("Session " + request.session.user);
 
 			response.json(person);
 		}
