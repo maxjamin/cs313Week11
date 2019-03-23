c6caf6d2debc90375b678fd0b427f3441519ab63
maxjamin <maxjamin@gmail.com>
2019-03-23 16:01:14 -0600

    add sessions

--- public/clientsideMain.js
+++ public/clientsideMain.js
@@ -92,13 +92,13 @@ function loadCartTable(products, productsOnCart) {
 		$("#cartTable tr").remove();
 		var table = document.getElementById("cartTable");
 
-		for(var i=0; i<=productsOnCart.amountOfObjectToCart.length; i++) {
+		for(var i=1; i<productsOnCart.amountOfObjectToCart.length; i++) {
 
-			if(productsOnCart.objectToCart[i]) {
+			if(productsOnCart.objectToCart[i-1]) {
 				var product = [productsOnCart.objectToCart[i], 
 				productsOnCart.amountOfObjectToCart[i],
 				"<button onclick='removeFromCart()'>+</button>" ];
-				var prod = table.insertRow(i);
+				var prod = table.insertRow(i-1);
 
 				//Add items to the row.
 				for(var k=0; k<3; k++) {
